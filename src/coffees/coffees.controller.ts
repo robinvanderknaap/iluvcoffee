47e1237052ab485858f93b907a5cab9fa02c0740
Robin van der Knaap <robin@skaele.nl>
2024-01-23 23:29:16 +0100

    Chapter 2 finished

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
+  @Get('flavors')
+  findAll(@Query() paginationQuery) {
+    return this.coffeeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return createCoffeeDto;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return updateCoffeeDto;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+  }
+}
