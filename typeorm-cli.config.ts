7a9d2b6479be818b2b832d311428098938ea15f7
Robin van der Knaap <robin@skaele.nl>
2024-01-31 21:19:26 +0100

    Type orm

---
+++ typeorm-cli.config.ts
@@ -0,0 +1,13 @@
+import { CoffeeRefactor1706729611717 } from 'src/migrations/1706729611717-CoffeeRefactor';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'postgres',
+  entities: [],
+  migrations: [CoffeeRefactor1706729611717],
+});
